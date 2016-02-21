294052b44e4f94189b2279f8c5f300bbe4a9a4b0
eleanorstrib <eleanor@sloan.mit.edu>
2016-02-21 11:50:12 -0800

    successfully pulling urls from page, added base for full url in print sstatement

--- webscraper.py
+++ webscraper.py
@@ -1,6 +1,17 @@
 import requests
-from bs4 import BeautifulSoup as soup
+import bs4
+import pprint
+
+BASE_URL = "http://pyvideo.org/"
+pp = pprint.PrettyPrinter(indent=2)
 
 response = requests.get('http://pyvideo.org/category/50/pycon-us-2014')
 
-print response.text
+# entire dom
+soup = bs4.BeautifulSoup(response.text)
+
+# specific section of dom
+title_links =  [BASE_URL + a.attrs.get ('href') for a in soup.select('div#video-summary-content a[href^="/video"]')]
+
+
+pp.pprint(title_links)
