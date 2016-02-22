a6bbb84b138dcb46a4ed4d8f19d6d4a0579bc637
eleanorstrib <eleanor@sloan.mit.edu>
2016-02-21 21:05:04 -0800

    basic functionality is working!  TODOS: break into functions, add comments, see if I can do more robust error handling and deal with spaces in company names

--- indeedws.py
+++ indeedws.py
@@ -1,8 +1,10 @@
 import requests
 import bs4
 BASE_URL = "http://www.indeed.com/cmp/"
+RATING_DENOMINATOR = 85.225
+AVAILABLE_STARS = 5
 
-company = "google"
+company = "pinterest"
 
 # this gives the response code (eg <Response [200]>)
 response = requests.get(BASE_URL + company)
@@ -11,15 +13,19 @@ soup = bs4.BeautifulSoup(response.text, 'html.parser')
 
 # add data to a dict of company ratings
 company_ratings = {}
-company_ratings['overall'] = [float(element.get_text()) for element in soup.select('div span.cmp-average-rating')][0]
 
-rating_categories = [dl.get_text() for dl in soup.select('dl#cmp-reviews-attributes dt')]
-rating_stars = [span.attrs.get('style') for span in soup.select('span.cmp-star-on')]
+if len(soup.select('dl#cmp-reviews-attributes dt')) != 0:
+	rating_categories = [dl.get_text() for dl in soup.select('dl#cmp-reviews-attributes dt')]
 
-for item in rating_stars:
-	item = item[7:-2]
-	print item
-# title_links =  [BASE_URL + a.attrs.get ('href') for a in soup.select('div#video-summary-content a[href^="/video"]')]
+	# need to grab pixel values from dom, splice, and turn into integers
+	rating_stars_raw = [span.attrs.get('style') for span in soup.select('span.cmp-star-on')]
+	rating_stars_clean = [float(item[7:-2]) for item in rating_stars_raw]
 
-print("rating_categories", rating_categories)
-print("rating_stars", rating_stars)
+	for item in rating_categories:
+		company_ratings[item] = round(((rating_stars_clean[rating_categories.index(item)]/RATING_DENOMINATOR) * AVAILABLE_STARS), 1)
+
+	company_ratings['Overall'] = [float(element.get_text()) for element in soup.select('div span.cmp-average-rating')][0]
+	print("company_ratings", company_ratings)
+else:
+	print("There are no ratings available for %s." % company.title())
+
