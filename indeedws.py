3c168a7875a1412e251f62df90f0d0f96c3d9837
eleanorstrib <eleanor@sloan.mit.edu>
2016-02-21 20:50:36 -0800

    worked out collecting and splicing pixels, need to put together with ratings in dict form

--- indeedws.py
+++ indeedws.py
@@ -14,6 +14,12 @@ company_ratings = {}
 company_ratings['overall'] = [float(element.get_text()) for element in soup.select('div span.cmp-average-rating')][0]
 
 rating_categories = [dl.get_text() for dl in soup.select('dl#cmp-reviews-attributes dt')]
+rating_stars = [span.attrs.get('style') for span in soup.select('span.cmp-star-on')]
+
+for item in rating_stars:
+	item = item[7:-2]
+	print item
+# title_links =  [BASE_URL + a.attrs.get ('href') for a in soup.select('div#video-summary-content a[href^="/video"]')]
 
 print("rating_categories", rating_categories)
-print("rate_ct", rate_ct)
+print("rating_stars", rating_stars)
